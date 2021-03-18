c879c8cb9bc0ded027323b4148a64778e4317a9b
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-03-18 14:37:33 +1300

    Implement cross platform support with AsyncStorage

--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -1,319 +1,281 @@
-import RecentSearches from "..";
-
-const memoryStorageEnvironment = {
-  after: () => void 0,
-  before: () => {
-    const spy = jest.spyOn(localStorage, "setItem");
-
-    spy.mockImplementationOnce(() => {
-      throw new Error("Dead");
-    });
-  },
-  label: "MemoryStorage"
-};
-
-const localStorageEnvironment = {
-  after: () => {
-    localStorage.clear();
-  },
-  before: () => void 0,
-  label: "LocalStorage"
-};
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import RecentSearches, { DEFAULT_STORAGE_KEY } from "..";
 
 const defaultTTL = 1000 * 60 * 60;
 
 describe("RecentSearches", () => {
-  [memoryStorageEnvironment /*localStorageEnvironment*/].map(environment => {
-    describe(environment.label, () => {
-      beforeEach(() => {
-        environment.before();
+  describe("setRecentSearch", () => {
+    it("saves to storage", async () => {
+      const searches = new RecentSearches();
+
+      const spy = jest.spyOn(AsyncStorage, "setItem");
+      await searches.setRecentSearch("test", { se: "test" });
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it("protects falsey queries", async () => {
+      const searches = new RecentSearches();
+      expect(await searches.setRecentSearch(undefined as any, {})).toEqual([]);
+      expect(await searches.setRecentSearch("", {})).toEqual([]);
+      expect(await searches.setRecentSearch(0 as any, {})).not.toEqual([]);
+    });
+
+    it("does not exceed limit", async () => {
+      const limit = 5;
+      const searches = new RecentSearches({ limit });
+      await searches.initializeStorageData();
+      const newSearches = new Array(limit + 10).fill("query");
+
+      const results = await Promise.all(
+        newSearches.map((search) => searches.setRecentSearch(search))
+      );
+
+      results.forEach((search) =>
+        expect(search.length).toBeLessThanOrEqual(limit)
+      );
+
+      expect(
+        (searches as any).RECENT_SEARCHES.some(
+          (search: any) => typeof search.timestamp !== "number"
+        )
+      ).toBe(false);
+    });
+
+    it("saves additional data", async () => {
+      const searches = new RecentSearches({ ttl: defaultTTL });
+
+      expect(
+        (await searches.setRecentSearch("query", { data: "something" }))[0].data
+      ).toEqual({ data: "something" });
+      expect(searches.getRecentSearches()[0].data).toEqual({
+        data: "something",
+      });
+    });
+
+    it("appends new queries to start", async () => {
+      const searches = new RecentSearches({ ttl: defaultTTL });
+      await searches.initializeStorageData();
+      const newSearches = ["query", "newQuery"];
+
+      await Promise.all(
+        newSearches.map((search) => {
+          searches.setRecentSearch(search);
+        })
+      );
+
+      expect((searches as any).RECENT_SEARCHES[0].query).toBe(newSearches[1]);
+      expect((searches as any).RECENT_SEARCHES[1].query).toBe(newSearches[0]);
+    });
+
+    it("handles duplicates", async () => {
+      const data = [
+        {
+          query: "new",
+          timestamp: new Date().getTime() - 1000 * 60 * 15,
+        },
+        {
+          query: "old",
+          timestamp: new Date().getTime() - 1000 * 60 * 60 * 20,
+        },
+        {
+          query: "new_2",
+          timestamp: new Date().getTime() - 1000 * 60 * 60 * 30,
+        },
+      ];
+
+      const searches = new RecentSearches({ namespace: "CUSTOM" });
+      await AsyncStorage.setItem("CUSTOM", JSON.stringify(data));
+      await searches.initializeStorageData();
+
+      await searches.setRecentSearch("old");
+      expect(searches.getRecentSearches().length).toBe(2);
+    });
+  });
+
+  describe("initializeStorageData", () => {
+    it("filters expired searches", async () => {
+      const searches = new RecentSearches({
+        limit: 5,
+        namespace: "CUSTOM",
+        ttl: 1000 * 60 * 60,
       });
 
-      afterEach(() => {
-        environment.after();
+      await searches.initializeStorageData();
+
+      const storedData = [
+        {
+          query: "new",
+          timestamp: new Date().getTime() - 1000 * 60 * 15,
+        },
+        {
+          query: "old",
+          timestamp: new Date().getTime() - 1000 * 60 * 60 * 2,
+        },
+      ];
+      await AsyncStorage.setItem("CUSTOM", JSON.stringify(storedData));
+      expect(await searches.initializeStorageData()).toEqual([storedData[0]]);
+    });
+
+    it("respects limits", async () => {
+      const searches = new RecentSearches({
+        limit: 2,
+        namespace: "CUSTOM",
+        ttl: 1000 * 60 * 60,
+      });
+      await searches.initializeStorageData();
+
+      const storedData = [
+        {
+          query: "new",
+          timestamp: new Date().getTime() - 1000 * 60 * 15,
+        },
+        {
+          query: "new_2",
+          timestamp: new Date().getTime() - 1000 * 60 * 15,
+        },
+        {
+          query: "new_3",
+          timestamp: new Date().getTime() - 1000 * 60 * 15,
+        },
+        {
+          query: "old",
+          timestamp: new Date().getTime() - 1000 * 60 * 60 * 2,
+        },
+      ];
+      await AsyncStorage.setItem("CUSTOM", JSON.stringify(storedData));
+      expect(await searches.initializeStorageData()).toEqual(
+        storedData.slice(0, 2)
+      );
+    });
+  });
+
+  describe("getRecentSearches", () => {
+    const storedData = [
+      {
+        query: "some_new",
+        data: undefined,
+        timestamp: new Date().getTime() - 1000 * 60 * 15,
+      },
+      {
+        query: "new",
+        data: undefined,
+        timestamp: new Date().getTime() - 1000 * 60 * 15,
+      },
+      {
+        query: "other",
+        data: undefined,
+        timestamp: new Date().getTime() - 1000 * 60 * 15,
+      },
+      {
+        query: "old",
+        data: undefined,
+        timestamp: new Date().getTime() - 1000 * 60 * 60 * 2,
+      },
+    ];
+
+    const searches = new RecentSearches({
+      namespace: "CUSTOM",
+      ttl: defaultTTL,
+    });
+
+    beforeAll(async () => {
+      await AsyncStorage.setItem("CUSTOM", JSON.stringify(storedData));
+      await searches.initializeStorageData();
+    });
+
+    it("returns all searches when no query is sent", () => {
+      expect(searches.getRecentSearches()).toEqual(storedData.slice(0, 3));
+    });
+
+    it("returns all searches when no query is sent ranked by indexOf", () => {
+      expect(searches.getRecentSearches("ne")).toEqual([
+        storedData[1],
+        storedData[0],
+      ]);
+    });
+
+    describe("rank_by", () => {
+      const storedData = [
+        {
+          query: "some_new",
+          timestamp: new Date().getTime() - 1000 * 60 * 10,
+        },
+        { query: "new", timestamp: new Date().getTime() - 1000 * 60 * 5 },
+        {
+          query: "other",
+          timestamp: new Date().getTime() - 1000 * 60 * 2,
+        },
+        {
+          query: "old",
+          timestamp: new Date().getTime() - 1000 * 60 * 60 * 2,
+        },
+      ];
+
+      it("TIME", async () => {
+        const searches = new RecentSearches({
+          ttl: defaultTTL,
+          namespace: "CUSTOM",
+          ranking: "TIME",
+        });
+
+        await AsyncStorage.setItem("CUSTOM", JSON.stringify(storedData));
+        await searches.initializeStorageData();
+
+        expect(searches.getRecentSearches("ne")).toEqual([
+          storedData[2],
+          storedData[1],
+          storedData[0],
+        ]);
       });
 
-      describe("setRecentSearch", () => {
-        it("saves to storage", () => {
-          const searches = new RecentSearches();
-
-          const spy = jest.spyOn((searches as any).STORAGE, "setItem");
-          searches.setRecentSearch("test", { se: "test" });
-          expect(spy).toHaveBeenCalledTimes(1);
+      it("PROXIMITY", async () => {
+        const searches = new RecentSearches({
+          ttl: defaultTTL,
+          namespace: "CUSTOM",
+          ranking: "PROXIMITY",
         });
 
-        it("protects falsey queries", () => {
-          const searches = new RecentSearches();
-          expect(searches.setRecentSearch(undefined as any, {})).toEqual([]);
-          expect(searches.setRecentSearch("", {})).toEqual([]);
-          expect(searches.setRecentSearch(0 as any, {})).not.toEqual([]);
-        });
+        await AsyncStorage.setItem("CUSTOM", JSON.stringify(storedData));
+        await searches.initializeStorageData();
 
-        it("does not exceed limit", () => {
-          const limit = 5;
-          const searches = new RecentSearches({ limit });
-          (searches as any).initializeStorageData();
-          const newSearches = new Array(limit + 10).fill("query");
-
-          newSearches.forEach(search => {
-            expect(searches.setRecentSearch(search).length).toBeLessThanOrEqual(
-              limit
-            );
-          });
-
-          expect(
-            (searches as any).RECENT_SEARCHES.some(
-              (search: any) => typeof search.timestamp !== "number"
-            )
-          ).toBe(false);
-        });
-
-        it("saves additional data", () => {
-          const searches = new RecentSearches({ ttl: defaultTTL });
-
-          expect(
-            searches.setRecentSearch("query", { data: "something" })[0].data
-          ).toEqual({ data: "something" });
-          expect(searches.getRecentSearches()[0].data).toEqual({
-            data: "something"
-          });
-        });
-
-        it("appends new queries to start", () => {
-          const searches = new RecentSearches({ ttl: defaultTTL });
-          (searches as any).initializeStorageData();
-          const newSearches = ["query", "newQuery"];
-
-          newSearches.forEach(search => {
-            searches.setRecentSearch(search);
-          });
-
-          expect((searches as any).RECENT_SEARCHES[0].query).toBe(
-            newSearches[1]
-          );
-          expect((searches as any).RECENT_SEARCHES[1].query).toBe(
-            newSearches[0]
-          );
-        });
-
-        it("handles duplicates", () => {
-          const data = [
-            {
-              query: "new",
-              timestamp: new Date().getTime() - 1000 * 60 * 15
-            },
-            {
-              query: "old",
-              timestamp: new Date().getTime() - 1000 * 60 * 60 * 20
-            },
-            {
-              query: "new_2",
-              timestamp: new Date().getTime() - 1000 * 60 * 60 * 30
-            }
-          ];
-
-          const searches = new RecentSearches({ namespace: "CUSTOM" });
-          (searches as any).STORAGE.setItem(data);
-          (searches as any).initializeStorageData();
-
-          searches.setRecentSearch("old");
-          expect(searches.getRecentSearches().length).toBe(2);
-        });
+        expect(searches.getRecentSearches("ne")).toEqual([
+          storedData[1],
+          storedData[0],
+        ]);
       });
 
-      describe("initializeStorageData", () => {
-        it("filters expired searches", () => {
-          const searches = new RecentSearches({
-            limit: 5,
-            namespace: "CUSTOM",
-            ttl: 1000 * 60 * 60
-          });
-
-          (searches as any).initializeStorageData();
-
-          const storedData = [
-            {
-              query: "new",
-              timestamp: new Date().getTime() - 1000 * 60 * 15
-            },
-            {
-              query: "old",
-              timestamp: new Date().getTime() - 1000 * 60 * 60 * 2
-            }
-          ];
-          (searches as any).STORAGE.setItem(storedData);
-          expect((searches as any).initializeStorageData()).toEqual([
-            storedData[0]
-          ]);
-        });
-
-        it("respects limits", () => {
-          const searches = new RecentSearches({
-            limit: 2,
-            namespace: "CUSTOM",
-            ttl: 1000 * 60 * 60
-          });
-          (searches as any).initializeStorageData();
-
-          const storedData = [
-            {
-              query: "new",
-              timestamp: new Date().getTime() - 1000 * 60 * 15
-            },
-            {
-              query: "new_2",
-              timestamp: new Date().getTime() - 1000 * 60 * 15
-            },
-            {
-              query: "new_3",
-              timestamp: new Date().getTime() - 1000 * 60 * 15
-            },
-            {
-              query: "old",
-              timestamp: new Date().getTime() - 1000 * 60 * 60 * 2
-            }
-          ];
-          (searches as any).STORAGE.setItem(storedData);
-          expect((searches as any).initializeStorageData()).toEqual(
-            storedData.slice(0, 2)
-          );
-        });
-      });
-
-      describe("getRecentSearches", () => {
+      it("PROXIMITY_AND_TIME", async () => {
         const storedData = [
           {
-            query: "some_new",
-            data: undefined,
-            timestamp: new Date().getTime() - 1000 * 60 * 15
+            query: "___query",
+            timestamp: new Date().getTime() - 1000 * 60 * 1,
           },
           {
-            query: "new",
-            data: undefined,
-            timestamp: new Date().getTime() - 1000 * 60 * 15
+            query: "_t_query",
+            timestamp: new Date().getTime() - 1000 * 60 * 30,
           },
           {
-            query: "other",
-            data: undefined,
-            timestamp: new Date().getTime() - 1000 * 60 * 15
+            query: "query",
+            timestamp: new Date().getTime() - 1000 * 60 * 55,
           },
           {
-            query: "old",
-            data: undefined,
-            timestamp: new Date().getTime() - 1000 * 60 * 60 * 2
-          }
+            query: "__query",
+            timestamp: new Date().getTime() - 1000 * 60 * 30,
+          },
         ];
 
         const searches = new RecentSearches({
           namespace: "CUSTOM",
-          ttl: defaultTTL
+          ranking: "PROXIMITY_AND_TIME",
+          ttl: 1000 * 60 * 60,
         });
 
-        (searches as any).STORAGE.setItem(storedData);
-        (searches as any).initializeStorageData();
+        await AsyncStorage.setItem("CUSTOM", JSON.stringify(storedData));
+        await searches.initializeStorageData();
 
-        it("returns all searches when no query is sent", () => {
-          expect(searches.getRecentSearches()).toEqual(storedData.slice(0, 3));
-        });
+        const results = searches.getRecentSearches("query").map((q) => q.query);
 
-        it("returns all searches when no query is sent ranked by indexOf", () => {
-          expect(searches.getRecentSearches("ne")).toEqual([
-            storedData[1],
-            storedData[0]
-          ]);
-        });
-
-        describe("rank_by", () => {
-          const storedData = [
-            {
-              query: "some_new",
-              timestamp: new Date().getTime() - 1000 * 60 * 10
-            },
-            { query: "new", timestamp: new Date().getTime() - 1000 * 60 * 5 },
-            {
-              query: "other",
-              timestamp: new Date().getTime() - 1000 * 60 * 2
-            },
-            {
-              query: "old",
-              timestamp: new Date().getTime() - 1000 * 60 * 60 * 2
-            }
-          ];
-
-          it("TIME", () => {
-            const searches = new RecentSearches({
-              ttl: defaultTTL,
-              namespace: "CUSTOM",
-              ranking: "TIME"
-            });
-
-            (searches as any).STORAGE.setItem(storedData);
-            (searches as any).initializeStorageData();
-
-            expect(searches.getRecentSearches("ne")).toEqual([
-              storedData[2],
-              storedData[1],
-              storedData[0]
-            ]);
-          });
-
-          it("PROXIMITY", () => {
-            const searches = new RecentSearches({
-              ttl: defaultTTL,
-              namespace: "CUSTOM",
-              ranking: "PROXIMITY"
-            });
-
-            (searches as any).STORAGE.setItem(storedData);
-            (searches as any).initializeStorageData();
-
-            expect(searches.getRecentSearches("ne")).toEqual([
-              storedData[1],
-              storedData[0]
-            ]);
-          });
-
-          it("PROXIMITY_AND_TIME", () => {
-            const storedData = [
-              {
-                query: "___query",
-                timestamp: new Date().getTime() - 1000 * 60 * 1
-              },
-              {
-                query: "_t_query",
-                timestamp: new Date().getTime() - 1000 * 60 * 30
-              },
-              {
-                query: "query",
-                timestamp: new Date().getTime() - 1000 * 60 * 55
-              },
-              {
-                query: "__query",
-                timestamp: new Date().getTime() - 1000 * 60 * 30
-              }
-            ];
-
-            const searches = new RecentSearches({
-              namespace: "CUSTOM",
-              ranking: "PROXIMITY_AND_TIME",
-              ttl: 1000 * 60 * 60
-            });
-
-            (searches as any).STORAGE.setItem(storedData);
-            (searches as any).initializeStorageData();
-
-            const results = searches
-              .getRecentSearches("query")
-              .map(q => q.query);
-
-            expect(results).toEqual([
-              "query",
-              "___query",
-              "__query",
-              "_t_query"
-            ]);
-          });
-        });
+        expect(results).toEqual(["query", "___query", "__query", "_t_query"]);
       });
     });
   });
--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,9 @@
-import MemoryStorage from "./MemoryStorage";
-import LocalStorage, { SafeLocalStorage } from "./SafeLocalStorage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import computeMatchScore from "./utils/score";
 import isValidQuery from "./utils/string";
+
+export const DEFAULT_STORAGE_KEY = "__RECENT_SEARCHES__";
 
 export interface ISearch {
   query: string;
@@ -25,26 +26,18 @@ export interface IRecentSearchesConfig {
   ranking?: RankingStrategy;
 }
 
-export type RecentSearchesStorage =
-  | SafeLocalStorage<ISearch[]>
-  | MemoryStorage<ISearch[]>;
-
 export class RecentSearches {
+  private readonly KEY: string;
   private readonly TTL: number;
   private readonly LIMIT: number;
-  private readonly STORAGE: RecentSearchesStorage;
   private readonly RANKING: RankingStrategy;
 
   private RECENT_SEARCHES: ISearch[] = [];
 
   constructor(config: IRecentSearchesConfig = {}) {
+    this.KEY = config.namespace ?? DEFAULT_STORAGE_KEY;
     this.TTL = config.ttl || 1000 * 60 * 60 * 24;
     this.LIMIT = config.limit || 50;
-    this.STORAGE = LocalStorage<ISearch[]>({
-      defaultValue: [],
-      key: config.namespace
-    });
-    this.RECENT_SEARCHES = this.initializeStorageData();
     this.RANKING = config.ranking || "PROXIMITY_AND_TIME";
   }
 
@@ -60,22 +53,22 @@ export class RecentSearches {
       return this.RECENT_SEARCHES;
     }
 
-    const matchedSearches = this.RECENT_SEARCHES.map(search => {
+    const matchedSearches = this.RECENT_SEARCHES.map((search) => {
       const score = computeMatchScore(search, query, this.RANKING, this.TTL);
 
       return {
         data: search.data,
         query: search.query,
         score,
-        timestamp: search.timestamp
+        timestamp: search.timestamp,
       };
     })
       .filter(this.filterScoredResults)
       .sort(this.sortScoredResults)
-      .map(search => ({
+      .map((search) => ({
         data: search.data,
         query: search.query,
-        timestamp: search.timestamp
+        timestamp: search.timestamp,
       }));
 
     return matchedSearches;
@@ -86,12 +79,12 @@ export class RecentSearches {
    *
    * @param  {string} query
    * @param  {object} data?
-   * @returns Search[]
+   * @returns Promise<Search[]>
    */
-  public setRecentSearch = (
+  public setRecentSearch = async (
     query: string | number,
     data?: ISearch["data"]
-  ): ISearch[] => {
+  ): Promise<ISearch[]> => {
     if (!isValidQuery(query)) {
       return this.RECENT_SEARCHES;
     }
@@ -99,11 +92,11 @@ export class RecentSearches {
     const search: ISearch = {
       data,
       query: String(query),
-      timestamp: new Date().getTime()
+      timestamp: new Date().getTime(),
     };
 
     const existingQueryIndex = this.RECENT_SEARCHES.findIndex(
-      searchEntry => searchEntry.query === query
+      (searchEntry) => searchEntry.query === query
     );
 
     if (existingQueryIndex > -1) {
@@ -112,9 +105,30 @@ export class RecentSearches {
 
     this.RECENT_SEARCHES.unshift(search);
     this.RECENT_SEARCHES = this.RECENT_SEARCHES.slice(0, this.LIMIT);
-    this.STORAGE.setItem(this.RECENT_SEARCHES);
+
+    await AsyncStorage.setItem(this.KEY, JSON.stringify(this.RECENT_SEARCHES));
 
     return this.RECENT_SEARCHES;
+  };
+
+  /**
+   * Initialize recent searches from storage
+   *
+   * @returns Promise<Search[]>
+   */
+  public initializeStorageData = async (): Promise<ISearch[]> => {
+    const currentTimestamp = new Date().getTime();
+
+    const rawValue = await AsyncStorage.getItem(this.KEY);
+    const parsedValue = JSON.parse(rawValue ?? JSON.stringify([])) as ISearch[];
+
+    const items = parsedValue
+      .filter((search) => search.timestamp + this.TTL >= currentTimestamp)
+      .slice(0, this.LIMIT);
+
+    await AsyncStorage.setItem(this.KEY, JSON.stringify(items));
+    this.RECENT_SEARCHES = items;
+    return items;
   };
 
   private filterScoredResults = (search: ScoredSearch): boolean => {
@@ -127,18 +141,6 @@ export class RecentSearches {
 
   private sortScoredResults = (a: ScoredSearch, b: ScoredSearch): number => {
     return a.score - b.score;
-  };
-
-  private initializeStorageData = (): ISearch[] => {
-    const currentTimestamp = new Date().getTime();
-
-    const items = this.STORAGE.getItem()
-      .filter(search => search.timestamp + this.TTL >= currentTimestamp)
-      .slice(0, this.LIMIT);
-
-    this.STORAGE.setItem(items);
-    this.RECENT_SEARCHES = items;
-    return items;
   };
 }
 
