90b050873ae97e3f14be7fd94959aba0804652e5
JonasBa <jonas@specto.dev>
2021-04-18 20:15:38 +0200

    feat(trie): add initial trie

--- src/SearchIndex.test.ts
+++ src/SearchIndex.test.ts
@@ -1,1 +1,49 @@
-describe("SearchIndex", () => {})
+import SearchIndex from "./SearchIndex"
+
+describe("getCommonPrefix", () => {
+
+})
+
+describe("TrieSearchIndex", () => {
+    describe("insert", () => {
+        // it('inserts root node',() => {
+        //     const index = new SearchIndex()
+        //     index.insert("wor", {id: 2})
+        //     index.insert("worm", {id: 1})
+        //     console.log("Trie", JSON.stringify(index.trie, null, 2))
+        // })
+
+        it.skip("inserts new node", () => {})
+        it.skip("inserts new node and handles collision", () => {})
+        it.skip("inserts new node and handles collision", () => {})
+    })
+
+    describe("remove", () => {
+        // it("removes node that has no siblings", () => {
+        //     const index = new SearchIndex()
+        //     index.insert("wor", {id: 2})
+        //     index.insert("worm", {id: 2})
+        //     index.insert("wors", {id: 2})
+        // })
+        
+        it.only("removes node that has siblings", () => {
+            const index = new SearchIndex({key: "id"})
+
+            index.insert("abcd", {id: 1})
+            index.insert("abcf", {id: 2})
+            
+            console.log("Before: Trie", index.toString())
+            index.remove("abc")
+            console.log("After: Trie", index.toString())
+
+            // const newIndex = new SearchIndex()
+            // newIndex.insert("worm_a", {id: 1})
+            // newIndex.insert("worm_b", {id: 1})
+            // console.log("Expected: Trie", JSON.stringify(newIndex.trie, null, 2))
+        })
+    })
+
+    describe("build", () => {
+        it.skip("builds new index", () => {})
+    })
+})
--- src/SearchIndex.ts
+++ src/SearchIndex.ts
@@ -1,22 +1,155 @@
 interface SearchIndex<T> {
-    // search: (query: string) => T[]
-    insert: (data: T) => void
-    remove: (data: T) => void;
-    build: (data: T[]) => void
+    insert: (string: string, data: T) => void
+    remove: (string: string) => void;
+    clear: () => void;
+}
+
+type Trie<T> = {[key: string]: Trie<T> }
+type KeyAccessor<T> = (entry: T) => string | number | symbol;
+interface TrieOptions<T> {
+    key: string | symbol | KeyAccessor<T>
+}
+class TrieIndex<T extends Record<any, any>> implements SearchIndex<T> {
+    private options: TrieOptions<T>;
+    private trie: Trie<T> = {};
+    private reverseIndex: Record<string, Record<ReturnType<typeof getKey>, T>> = {}
+
+    constructor(options: TrieOptions<T>) {
+        this.options = options
+    }
+
+    public insert(string: string, data: T): void {
+        if(!this.reverseIndex[string]) this.reverseIndex[string] = {}
+        this.reverseIndex[string][getKey(this.options.key, data)] = data
+
+        this.trieInsert(string, data, this.trie)
+    }
+
+    public remove(string: string) {
+        delete this.reverseIndex[string]
+        this.trieRemove(string, this.trie, null)
+    }
+
+    public clear(){
+        this.reverseIndex = {}
+        this.trie = {}
+    }
+
+    private trieInsert(string: string, data: T, tree: Trie<T>) {
+        let length = string.length;
+        let prefix: string;
+
+        while(length--) {
+            prefix = string.substr(0, length + 1)
+            const match = tree[prefix]
+
+            if(match){
+                // Found prefix, moving into subtrie
+                // if (!Object.keys(tree[prefix]).length) {
+                //     // If one word is a pure subset of another word, the prefix 
+                //     // should also point to the subset.
+                //     tree[prefix][""] = {};
+                // }
+                const carryOverPrefix = string.substr(length+1)
+                this.trieInsert(carryOverPrefix, data, match)
+                return
+            }
+        }
+
+        const siblings = Object.keys(tree);
+
+        const siblingFound = siblings.some((sibling) => {
+            const [commonPrefix, distance] = getCommonPrefixForNode(string, sibling, )
+
+            if(commonPrefix === null || distance === null) return false
+
+            // Clear current prefix and ensure we start fresh
+            tree[commonPrefix] = {};
+            this.trieInsert(sibling.substr(distance), data, tree[commonPrefix]);
+            tree[commonPrefix][sibling.substr(distance)] = tree[sibling];
+            this.trieInsert(string.substr(distance), data, tree[commonPrefix]);
+
+            delete tree[sibling];
+            return true;
+        });
+
+        if(!siblingFound){
+            console.log("Insert", string, data)
+            tree[string] = {}
+        }
+    }
+
+    private trieRemove(string: string, tree: Trie<T>, parent: Trie<T>|null): void{
+        let length = string.length;
+        let prefix: string;
+
+        while(length--) {
+            prefix = string.substr(0, length + 1)
+            const match = tree[prefix]
+
+            if(match){
+                const carryOverPrefix = string.substr(length+1)
+
+                if(!carryOverPrefix && tree[prefix]) {
+                    const siblings = Object.keys(tree[prefix])
+
+                    if(!siblings.length) {
+                        delete tree[prefix]
+                        return
+                    }
+
+                    
+                    const children = tree[prefix]
+
+                    if(parent){
+                        delete tree[prefix]
+                        Object.keys(children).map(c => {
+                            tree[prefix+c] = children[c]
+                        })
+                    }
+                }
+
+                return this.trieRemove(carryOverPrefix, match, tree)
+            }
+        }
+    }
+
+    public toString(){
+        return JSON.stringify(this.trie, null, 2)
+    }
+}
+
+function getKey<T extends Record<any, any>>( key: TrieOptions<T>["key"], data: T): string | number{
+    let primaryKey: unknown | undefined;
+
+    // @ts-ignore https://github.com/Microsoft/TypeScript/issues/24587
+    if(typeof key === "string" || typeof key === "symbol") primaryKey = data[key] as string;
+    if(typeof key === "function") primaryKey = key(data)
+
+    if(typeof primaryKey === "undefined") {
+        throw new Error(`Could not retrieve primaryKey from ${JSON.stringify(data)} - got ${primaryKey}`)
+    }
+
+    if(typeof primaryKey === "string" || typeof primaryKey === "number" || typeof primaryKey === "symbol"){
+        return primaryKey as string | number
+    }
+    
+    throw new TypeError(`PrimaryKey needs to be either a string, symbol or number, got ${JSON.stringify(primaryKey)}`)
+}
+
+function getCommonPrefixForNode(sibling: string, prefix: string): [string, number] | [null, null] {
+    let s = 0;
+    let commonPrefix = null;
+
+    while (sibling[s] === prefix[s]) {
+        s++;
+        if(s >= 1){
+            commonPrefix = sibling.substr(0, s)
+        }
+    }
+
+    return commonPrefix ? [commonPrefix, s] : [null, null]
 }
 
 
-type TrieNode<T> = T
-type Trie<T> = Record<string, TrieNode<T>>
-
-class TrieIndex<T> implements SearchIndex<T> {
-    insert(data: T) {
-
-    }
-    remove(data: T) {
-
-    }
-    build(data: T[]) {
-
-    }
-}
+export default TrieIndex
